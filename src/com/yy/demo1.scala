b16859aedd66deee93c5223def8ea1ededa5bc7d
lvmao520 <983084772@qq.com>
2022-11-08 09:47:20 +0800

    First for ScalaDemo test.

---
+++ src/com/yy/demo1.scala
@@ -0,0 +1,14 @@
+package com.yy
+
+object demo1 {
+  def main(args: Array[String]): Unit = {
+    println("hello,world")
+
+    val name="张三"
+    println(name)
+
+    var age=30
+    age=16
+    println(age)
+  }
+}
